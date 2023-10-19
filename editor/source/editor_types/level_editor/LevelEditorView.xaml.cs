edfc71ca856fe4cab3e0fc2a1f3b38fbde7c3ea9
Matheus Marchetti <matheusrmarchetti@gmail.com>
2023-10-19 00:50:07 -0300

    Added project files to editor

---
+++ editor/source/editor_types/level_editor/LevelEditorView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using editor.game_development;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace editor.editor_types
+{
+    public partial class LevelEditorView : UserControl
+    {
+        public LevelEditorView()
+        {
+            InitializeComponent();
+            Loaded += OnLevelEditorLoaded;
+        }
+
+        private void OnLevelEditorLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= OnLevelEditorLoaded;
+            Focus();
+        }
+
+        private void OnNewScript_Click(object sender, RoutedEventArgs e)
+        {
+            new NewScriptDialog().ShowDialog();
+        }
+    }
+}
---
+++ editor/source/editor_types/level_editor/ScriptView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace editor.editor_types
+{
+    public partial class ScriptView : UserControl
+    {
+        public ScriptView()
+        {
+            InitializeComponent();
+        }
+    }
+}
