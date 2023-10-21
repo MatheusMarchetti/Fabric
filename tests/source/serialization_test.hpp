6dce473ff8d16649757c9e22bec75b54dcc58975
Matheus Marchetti <matheusrmarchetti@gmail.com>
2023-10-20 22:55:52 -0300

    Improved serialization/deserialization tests
    - Works with multiple components
    - Works even if not every entity has every component
    - Retrieves data correctly

--- tests/source/serialization_test.hpp
+++ tests/source/serialization_test.hpp
@@ -5,7 +5,6 @@
 #include "include/fabric.hpp"
 
 #include <iostream>
-#include <typeinfo>
 
 using namespace fabric;
 
@@ -20,6 +19,16 @@ struct Transform
 	}
 };
 
+struct Audio
+{
+	float volume;
+};
+
+struct Geometry
+{
+	u32 mesh_id;
+};
+
 void serialize()
 {
 	for (u32 i = 0; i < 25; i++)
@@ -29,8 +38,20 @@ void serialize()
 		t.position[1] = 1.0f;
 		t.position[2] = 2.0f;
 
+		Audio a;
+		a.volume = 10.0f;
+
+		Geometry g;
+		g.mesh_id = 7;
+
 		ecs::entity e = ecs::create_entity();
 		e.add_component<Transform>(t);
+
+		if(i % 2)
+			e.add_component<Audio>(a);
+
+		if(i % 5)
+			e.add_component<Geometry>(g);
 	}
 
 	ecs::save_scene();
@@ -41,14 +62,23 @@ class engine_test : public test
 public:
 	virtual bool initialize() override
 	{
-		serialize();
+		//serialize();
 
 		return ecs::load_scene();
 	}
 
 	virtual void run() override
 	{
-		std::cout << "Transform ID via typeid::hash_code(): " << typeid(Transform).hash_code() << std::endl;
+		auto entities = ecs::get_entities_with<Transform>();
+
+		for (auto& entity : entities)
+		{
+			Transform& t = entity.get_component<Transform>();
+			std::cout << t.position[0] << ", " << t.position[1] << ", " << t.position[2] << ", " << t.rotation << std::endl;
+		}
+
+		entities = ecs::get_entities_with<Audio>();
+		entities = ecs::get_entities_with<Geometry>();
 
 		std::cin.get();
 	}
