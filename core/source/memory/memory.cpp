f516116ff1b78d7d07f0d220d20c055d3e5f70e8
Matheus Marchetti <matheusrmarchetti@gmail.com>
2023-10-25 00:32:43 -0300

    Modified pool allocator interface and added raw_allocator
    - Made changes to all allocators (removed void* memory from constructor). Block will request memory internally from the dynamic allocator

--- core/source/memory/memory.cpp
+++ core/source/memory/memory.cpp
@@ -9,10 +9,11 @@ namespace fabric::memory
 
     }
 
-    linear_block::linear_block(void* memory, u32 size)
-        : m_block(memory), m_size(size), m_offset(0)
+    linear_block::linear_block(u32 size)
+        : m_size(size), m_offset(0)
     {
-        memset(m_block, 0, m_size);
+        m_block = malloc(m_size);
+        memset(m_block, -1, m_size);
     }
 
     void* linear_block::allocate(u32 size)
@@ -26,16 +27,16 @@ namespace fabric::memory
         return memory;
     }
 
-    void linear_block::free()
+    void linear_block::free() const
     {
         ::free(m_block);
-        m_block = nullptr;
     }
 
-    stack_block::stack_block(void* memory, u32 size)
-        : m_block(memory), m_size(size), m_offset(0)
+    stack_block::stack_block(u32 size)
+        : m_size(size), m_offset(0)
     {
-        memset(m_block, 0, m_size);
+        m_block = malloc(m_size);
+        memset(m_block, -1, m_size);
     }
 
     void* stack_block::allocate(u32 size)
@@ -55,29 +56,72 @@ namespace fabric::memory
         return memory;
     }
 
-    void* stack_block::deallocate(void* memory)
+    void stack_block::deallocate(void* memory)
     {
         header* h = (header*)((char*)memory - sizeof(header));
         u32 size = h->allocation_size;
         m_offset -= size + sizeof(header);
-
-        return (char*)m_block + m_offset;
     }
 
-    void stack_block::free()
+    void stack_block::free() const
     {
         ::free(m_block);
-        m_block = nullptr;
+    }
+
+    pool_block::pool_block(u32 count, u32 chunk_size)
+        : m_size(count * chunk_size), m_chunk_size(chunk_size)
+    {
+        m_size = count * (chunk_size + sizeof(header));
+
+        // TODO: Request from dynamic memory allocator
+        ::free(m_block);
+        m_block = malloc(m_size);
+
+        void* memory = (char*)m_block;
+        for (u32 offset = 0; offset < m_size; offset += (chunk_size + sizeof(header)))
+        {
+            header* h = (header*)((char*)memory + offset);
+            
+            u32 next_offset = offset + (chunk_size + sizeof(header));
+
+            if (next_offset > m_size)
+                h->next = (header*)m_block;
+            else
+                h->next = (header*)m_block + next_offset;
+        }
+
+        m_free = (header*)m_block;
+
+        memset(m_block, -1, m_size);
+    }
+
+    void* pool_block::allocate()
+    {
+        void* memory = (void*)(m_free + sizeof(header));
+        m_free = m_free->next;
+
+        return memory;
+    }
+
+    void pool_block::deallocate(void* memory)
+    {
+        header* h = (header*)((char*)memory - sizeof(header));
+        h->next = m_free;
+    }
+
+    void pool_block::free() const
+    {
+        ::free(m_block);
     }
 
     namespace linear_allocator
     {
         linear_block request_block(u32 block_size)
         {
-            return linear_block(malloc(block_size), block_size);
+            return linear_block(block_size);
         }
 
-        void return_block(linear_block& block)
+        void return_block(const linear_block& block)
         {
             block.free();
         }
@@ -87,10 +131,10 @@ namespace fabric::memory
     {
         stack_block request_block(u32 block_size)
         {
-            return stack_block(malloc(block_size), block_size);
+            return stack_block(block_size);
         }
 
-        void return_block(stack_block& block)
+        void return_block(const stack_block& block)
         {
             block.free();
         }
@@ -98,15 +142,28 @@ namespace fabric::memory
 
     namespace pool_allocator
     {
-        void* allocate(size_t size)
+        pool_block request_block(u32 count, u32 chunk_size)
+        {
+            return pool_block(count, chunk_size);
+        }
+        void return_block(const pool_block& block)
+        {
+            block.free();
+        }
+    }
+
+    namespace raw_allocator
+    {
+        void* request_raw(u32 size)
         {
             return malloc(size);
         }
 
-        void deallocate(void* block)
+        void return_raw(void* block)
         {
             ::free(block);
             block = nullptr;
         }
     }
 }
+
