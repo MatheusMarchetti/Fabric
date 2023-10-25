0442b832ef71b5af88dc7a21fa207cf29888a3c6
Matheus Marchetti <matheusrmarchetti@gmail.com>
2023-10-25 00:43:33 -0300

    Revert "Modified pool allocator interface and added raw_allocator"
    This reverts commit 84142c3987277dbd5177c92b7c0329e3bd1a7563.

--- core/source/memory/memory.cpp
+++ core/source/memory/memory.cpp
@@ -9,11 +9,9 @@ namespace fabric::memory
 
     }
 
-    linear_block::linear_block(u32 size)
-        : m_size(size), m_offset(0)
+    void* pool_allocator::allocate(size_t size)
     {
-        m_block = malloc(m_size);
-        memset(m_block, -1, m_size);
+        return malloc(size);
     }
 
     void pool_allocator::deallocate(void* block)
@@ -22,143 +20,15 @@ namespace fabric::memory
         block = nullptr;
     }
 
-    void linear_block::free() const
+    void* linear_allocator::allocate(size_t size)
     {
-        ::free(m_block);
+        return malloc(size);
     }
 
-    stack_block::stack_block(u32 size)
-        : m_size(size), m_offset(0)
+    void linear_allocator::deallocate(void* block)
     {
-        m_block = malloc(m_size);
-        memset(m_block, -1, m_size);
-    }
-
-    void* stack_block::allocate(u32 size)
-    {
-        if (m_offset + size + sizeof(header) > m_size)
-            return nullptr;
-        
-        header h;
-        h.allocation_size = size;
-
-        void* memory = (char*)m_block + m_offset;
-        m_offset += sizeof(header);
-        *(header*)memory = h;
-        memory = (char*)memory + m_offset;
-        m_offset += size;
-
-        return memory;
-    }
-
-    void stack_block::deallocate(void* memory)
-    {
-        header* h = (header*)((char*)memory - sizeof(header));
-        u32 size = h->allocation_size;
-        m_offset -= size + sizeof(header);
-    }
-
-    void stack_block::free() const
-    {
-        ::free(m_block);
-    }
-
-    pool_block::pool_block(u32 count, u32 chunk_size)
-        : m_size(count * chunk_size), m_chunk_size(chunk_size)
-    {
-        m_size = count * (chunk_size + sizeof(header));
-
-        // TODO: Request from dynamic memory allocator
-        ::free(m_block);
-        m_block = malloc(m_size);
-
-        void* memory = (char*)m_block;
-        for (u32 offset = 0; offset < m_size; offset += (chunk_size + sizeof(header)))
-        {
-            header* h = (header*)((char*)memory + offset);
-            
-            u32 next_offset = offset + (chunk_size + sizeof(header));
-
-            if (next_offset > m_size)
-                h->next = (header*)m_block;
-            else
-                h->next = (header*)m_block + next_offset;
-        }
-
-        m_free = (header*)m_block;
-
-        memset(m_block, -1, m_size);
-    }
-
-    void* pool_block::allocate()
-    {
-        void* memory = (void*)(m_free + sizeof(header));
-        m_free = m_free->next;
-
-        return memory;
-    }
-
-    void pool_block::deallocate(void* memory)
-    {
-        header* h = (header*)((char*)memory - sizeof(header));
-        h->next = m_free;
-    }
-
-    void pool_block::free() const
-    {
-        ::free(m_block);
-    }
-
-    namespace linear_allocator
-    {
-        linear_block request_block(u32 block_size)
-        {
-            return linear_block(block_size);
-        }
-
-        void return_block(const linear_block& block)
-        {
-            block.free();
-        }
-    }
-
-    namespace stack_allocator
-    {
-        stack_block request_block(u32 block_size)
-        {
-            return stack_block(block_size);
-        }
-
-        void return_block(const stack_block& block)
-        {
-            block.free();
-        }
-    }
-
-    namespace pool_allocator
-    {
-        pool_block request_block(u32 count, u32 chunk_size)
-        {
-            return pool_block(count, chunk_size);
-        }
-        void return_block(const pool_block& block)
-        {
-            block.free();
-        }
-    }
-
-    namespace raw_allocator
-    {
-        void* request_raw(u32 size)
-        {
-            return malloc(size);
-        }
-
-        void return_raw(void* block)
-        {
-            ::free(block);
-            block = nullptr;
-        }
+        ::free(block);
+        block = nullptr;
     }
 }
 
