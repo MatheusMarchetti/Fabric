84142c3987277dbd5177c92b7c0329e3bd1a7563
Matheus Marchetti <matheusrmarchetti@gmail.com>
2023-10-25 00:32:43 -0300

    Modified pool allocator interface and added raw_allocator
    - Made changes to all allocators (removed void* memory from constructor). Block will request memory internally from the dynamic allocator
    
    # Conflicts:
    #	core/source/memory/memory.cpp
    #	core/source/memory/memory.hpp

--- core/source/memory/memory.hpp
+++ core/source/memory/memory.hpp
@@ -2,10 +2,92 @@
 
 namespace fabric::memory
 {
+	class linear_block
+	{
+	public:
+		linear_block() = default;
+		linear_block(u32 size);
+
+		void* allocate(u32 size);
+
+		// temporary
+		void free() const;
+
+	private:
+		void* m_block = nullptr;
+		u32 m_size = 0;
+		u32 m_offset = 0;
+	};
+
+	class stack_block
+	{
+	public:
+		stack_block() = default;
+		stack_block(u32 size);
+		void* allocate(u32 size);
+		void deallocate(void* memory);
+
+		// temporary
+		void free() const;
+
+	private:
+		struct header
+		{
+			u32 allocation_size;
+		};
+
+	private:
+		void* m_block = nullptr;
+		u32 m_size = 0;
+		u32 m_offset = 0;
+	};
+
+	class pool_block
+	{
+	public:
+		pool_block() = default;
+		pool_block(u32 count, u32 chunk_size);
+		void* allocate();
+		void deallocate(void* memory);
+
+		// temporary
+		void free() const;
+
+	private:
+		struct header
+		{
+			header* next;
+		};
+
+	private:
+		void* m_block = nullptr;
+		header* m_free = nullptr;
+		u32 m_size = 0;
+		u32 m_chunk_size = 0;
+	};
+
+	namespace linear_allocator
+	{
+		linear_block request_block(u32 block_size);
+		void return_block(const linear_block& block);
+	}
+
+	namespace stack_allocator
+	{
+		stack_block request_block(u32 block_size);
+		void return_block(const stack_block& block);
+	}
+
 	namespace pool_allocator
 	{
-		void* allocate(size_t size);
-		void deallocate(void* block);
+		pool_block request_block(u32 count, u32 chunk_size);
+		void return_block(const pool_block& block);
+	}
+
+	namespace raw_allocator
+	{
+		void* request_raw(u32 size);
+		void return_raw(void* block);
 	}
 
 	namespace linear_allocator
